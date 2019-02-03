bcae542af95544bd6493894cc63d41f9b08be9df
Antonio Campos <antonio.hdez93@gmail.com>
2019-02-03 00:36:28 -0600

    Build system.

---
+++ Gulpfile.js
@@ -0,0 +1,63 @@
+const gulp = require( 'gulp' );
+const del = require( 'del' );
+const cssnano = require('gulp-cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require("gulp-babel");
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
+const browserSync = require( 'browser-sync' ).create();
+
+gulp.task( 'css', function() {
+    del.sync([ './build/assets/css' ]);
+
+    return gulp.src( './assets/css/**/*.css' )
+        .pipe( sourcemaps.init() )
+        .pipe( cssnano() )
+        .pipe( sourcemaps.write( '.' ) )
+        .pipe( gulp.dest( './build/assets/css' ) );
+});
+
+gulp.task( 'js', function() {
+    del.sync([ './build/assets/js' ]);
+
+    return gulp.src( './assets/js/**/*.js' )
+        .pipe( sourcemaps.init() )
+        .pipe( babel({ presets: [ '@babel/env' ] }) )
+        .pipe( uglify() )
+        .pipe( concat( 'main.js' ) )
+        .pipe( sourcemaps.write( '.' ) )
+        .pipe( gulp.dest( './build/assets/js' ) );
+});
+
+gulp.task( 'img', function() {
+    del.sync([ './build/assets/img' ]);
+
+    return gulp.src( './assets/img/**/*.{png,jpg,jpeg,gif,svg}' )
+        .pipe( imagemin({ verbose: true }) )
+        .pipe( gulp.dest( './build/assets/img' ) );
+});
+
+gulp.task( 'html', function() {
+    del.sync([ './build/*.html', './build/assets/**/*.html', '!./build/assets' ]);
+
+    return gulp.src( [ './*.html', './assets/**/*.html' ] )
+        .pipe( htmlmin({ collapseWhitespace: true, removeComments: true }) )
+        .pipe( gulp.dest( './build' ) );
+});
+
+gulp.task( 'build', gulp.parallel( 'css', 'js', 'img', 'html' ) );
+
+gulp.task( 'watch', function() {
+    gulp.watch( './assets/css/**/*.css', gulp.series( 'css' ) );
+    gulp.watch( './assets/js/**/*.js', gulp.series( 'js' ) );
+    gulp.watch( './assets/img/**/*.img', gulp.series( 'img' ) );
+    gulp.watch( [ './*.html', './assets/**/*.html' ], gulp.series( 'html' ) );
+
+    browserSync.init({
+        server: './build'
+    });
+
+    gulp.watch( './build' ).on( 'change', browserSync.reload );
+});
